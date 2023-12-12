df5da9f41f180515eef2185ed6ef6fec40585f6b
Praises003 <praiseabiodun15@gmail.com>
2023-12-12 13:00:17 +0100

    updates

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ connectDb()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors({
+  origin: 'https://joof.onrender.com/'
+}));
 
 // app.use(fileUpload({
 //     useTempFiles: true,
