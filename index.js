ccae984a52691f4d60d929f9497356d0fcbda53f
Praises003 <praiseabiodun15@gmail.com>
2023-12-21 14:14:12 +0100

    updates

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ connectDb()
 //    credentials: true,
 //  };
 const app = express()
+if(process.env.NODE_ENV !== "development") {
+  app.use(cors())
+}
 //app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
