0dd6d8599fdd9f12ef590eb5ff1ec4976aac7f63
Praises003 <praiseabiodun15@gmail.com>
2023-12-21 17:24:52 +0100

    updates

--- index.js
+++ index.js
@@ -11,15 +11,18 @@ const eventRoute = require("./routes/eventRoute")
 
 
 connectDb()
-// const corsOptions = {
-//    origin: ['http://localhost:3000'],
-//    credentials: true,
-//  };
+const corsOptions = {
+   origin: ['http://localhost:3000'],
+   credentials: true,
+ };
 const app = express()
 if(process.env.NODE_ENV !== "development") {
-  app.use(cors())
+  app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  }))
 }
-//app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
