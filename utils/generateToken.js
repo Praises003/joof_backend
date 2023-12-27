022b671e19b65ec21491c1a1585ce1c3609b3c18
Praises003 <praiseabiodun15@gmail.com>
2023-12-27 10:49:07 +0100

    updates

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -14,6 +14,7 @@ const generateToken = (res, userId) => {
         secure: process.env.NODE_ENV !== 'development', 
         sameSite: 'Lax',
         maxAge: 30 * 24 * 60 * 60 * 1000,
+        path: "/",
         //domain: 'http://localhost:5000'
         //domain: 'https://joof-backend.onrender.com'
     })
