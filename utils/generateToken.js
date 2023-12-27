89a0a7864c9ca4dd7021e7f951e12ffdc028945e
Praises003 <praiseabiodun15@gmail.com>
2023-12-27 09:43:17 +0100

    env updates

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -6,12 +6,14 @@ const generateToken = (res, userId) => {
         expiresIn: "30d"
     })
 
+    
+
 
     res.cookie('auth', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development', 
         sameSite: 'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000,
+        maxAge: 30 * 24 * 60 * 60 * 1000
     })
 
     //console.log(process.env.NODE_ENV)
