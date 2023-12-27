44c0a7212564adf394026510d73227297dc03d75
Praises003 <praiseabiodun15@gmail.com>
2023-12-27 10:34:26 +0100

    updates

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -12,10 +12,10 @@ const generateToken = (res, userId) => {
     res.cookie('auth', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development', 
-        sameSite: 'strict',
+        sameSite: 'Lax',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         //domain: 'http://localhost:5000'
-        domain: 'https://joof-backend.onrender.com'
+        //domain: 'https://joof-backend.onrender.com'
     })
 
     //console.log(process.env.NODE_ENV)
