fe1fb01e13fcec8da2bb5fa48a3a0b2b286de859
Praises003 <praiseabiodun15@gmail.com>
2023-12-28 01:16:54 +0100

    updates

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -12,7 +12,7 @@ const generateToken = (res, userId) => {
     res.cookie('auth', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development', 
-        sameSite: 'strict',
+        sameSite: process.env.NODE_ENV !== "development" ? "none": 'strict',
         maxAge: 30 * 24 * 60 * 60 * 1000,
         path: "/", 
         //domain: 'http://localhost:5000'
