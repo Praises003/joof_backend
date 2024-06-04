fa7f4be4e794578a808210cafdf5eeeb3a907ffd
Praises003 <praiseabiodun15@gmail.com>
2024-06-04 14:14:32 +0100

    gallery updates

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -3,6 +3,8 @@ const path = require('path')
 const fs = require('fs')
 const Image = require('../models/imageModel')
 const imageDownloader = require('image-downloader');
+const ImageI = require('../models/imageIModel');
+const ImageII = require('../models/imageIIModel');
 
 
 const PORT = process.env.PORT || 8000
@@ -31,6 +33,33 @@ const imageUpload = asyncHandler(async(req, res) => {
     //const url = `http://localhost:${PORT}/${req.file.path}`
     res.status(200).send({ message: 'File uploaded successfully',  savedImage });
   })
+
+  const imageUploadCon = asyncHandler(async(req, res) => {
+    if (!req.file) {
+      return res.status(400).send({ message: 'No file uploaded' });
+    }
+    const baseUrl = `${req.protocol}://${req.get('host')}`
+
+    let singleImage = {
+        url: `${baseUrl}/${req.file.path}`,
+        fileName: req.file.originalname
+      }
+
+     // Create a new image document
+    const newImage = new Image({
+        singleImage: singleImage,
+        //multipleImages: multipleImageUrls,
+        // Other model properties (if any)
+      });
+  
+      // Save the image document
+       const savedImage = await newImage.save();
+    //   console.log(savedImage.singleImage)
+    
+    //const url = `http://localhost:${PORT}/${req.file.path}`
+    res.status(200).send({ message: 'File uploaded successfully',  savedImage });
+  })
+
 
  
 
@@ -123,6 +152,180 @@ const uploadByLink = async(req, res) => {
 
 }
 
+const getAllImages = asyncHandler(async(req, res) => {
+    const images = await Image.findOne().sort({ createdAt: -1 });
+    if(images) {
+        res.status(200).json(images)
+    }
+    else {
+        res.status(400)
+        throw new Error("Images cannot be found")
+    }
+})
+
+// Get Multiple Images
+const getMultiImage = asyncHandler(async(req, res) => {
+  try {
+  const images = await Image.find()
+  const multipleImages = images.map(image => image.multipleImages).flat();
+
+  if (multipleImages.length > 0) {
+    res.status(200).json(multipleImages);
+  } else {
+    res.status(404).json({ message: 'No multiple images found' });
+  } 
+}  catch (error) {
+  res.status(500).json({ message: 'Server error', error: error.message });
+}
+});
 
 
-  module.exports = {imageUpload,multiImageUpload ,uploadByLink}
+// ImageI Controller
+const imageUploadI = asyncHandler(async(req, res) => {
+    if (!req.file) {
+      return res.status(400).send({ message: 'No file uploaded' });
+    }
+    const baseUrl = `${req.protocol}://${req.get('host')}`
+
+    let singleImage = {
+        url: `${baseUrl}/${req.file.path}`,
+        fileName: req.file.originalname
+      }
+
+     // Create a new image document
+    const newImage = new ImageI({
+        singleImage: singleImage,
+        //multipleImages: multipleImageUrls,
+        // Other model properties (if any)
+      });
+  
+      // Save the image document
+       const savedImage = await newImage.save();
+    //   console.log(savedImage.singleImage)
+    
+    //const url = `http://localhost:${PORT}/${req.file.path}`
+    res.status(200).send({ message: 'File uploaded successfully',  savedImage });
+  })
+
+  // ImageI multiupload
+  const multiImageUploadI = asyncHandler(async(req, res) => {
+    //console.log(req.files)
+    if(!req.files  || req.files.length === 0) {
+        //console.log(req.files)
+        return res.status(400).send({ message: 'No file uploaded' }); 
+    }
+
+    const baseUrl = `${req.protocol}://${req.get('host')}`
+
+
+    // Map through the files to extract their paths
+    let multipleImg;
+    const images = req.files.map(file => (
+        multipleImg = {
+            url: `${baseUrl}/${file.path}`,
+            fieldName: file.originalname
+        }
+    ));
+
+    
+
+    
+
+    // Create a new image document
+    const newImage = new ImageI({
+        multipleImages: images,
+      });
+  
+      // Save the image document
+      const savedImage = await newImage.save();
+    
+    // Send a single response with all file paths
+    res.status(200).send({ message: 'Files uploaded successfully', filePaths: savedImage});
+
+    // const images = req.files.map(file =>{ (console.log(file))
+    // return res.send([file.path])
+    // res.status(200).send({ message: 'File uploaded successfully', filePath: [file]});
+//})
+    //res.send(images)
+
+    // for (let i = 0; i < req.files.length; i++) {
+    //     if(req.files[i].mimetype !== "image/jpeg") {
+
+    //     }
+    // }
+    
+})
+
+//
+
+// Image I Get Single Image
+const getImagesI = asyncHandler(async(req, res) => {
+  const images = await ImageI.findOne().sort({ createdAt: -1 });
+  if(images) {
+      res.status(200).json(images)
+  }
+  else {
+      res.status(400)
+      throw new Error("Images cannot be found")
+  }
+})
+
+// Get Multi ImageI
+const getMultiImageI = asyncHandler(async(req, res) => {
+  try {
+  const images = await ImageI.find()
+  const multipleImages = images.map(image => image.multipleImages).flat();
+
+  if (multipleImages.length > 0) {
+    res.status(200).json(multipleImages);
+  } else {
+    res.status(404).json({ message: 'No multiple images found' });
+  } 
+}  catch (error) {
+  res.status(500).json({ message: 'Server error', error: error.message });
+}
+});
+
+
+
+  // ImageII Controller
+  const imageUploadII = asyncHandler(async(req, res) => {
+    if (!req.file) {
+      return res.status(400).send({ message: 'No file uploaded' });
+    }
+    const baseUrl = `${req.protocol}://${req.get('host')}`
+
+    let singleImage = {
+        url: `${baseUrl}/${req.file.path}`,
+        fileName: req.file.originalname
+      }
+
+     // Create a new image document
+    const newImage = new ImageII({
+        singleImage: singleImage,
+        //multipleImages: multipleImageUrls,
+        // Other model properties (if any)
+      });
+  
+      // Save the image document
+       const savedImage = await newImage.save();
+    //   console.log(savedImage.singleImage)
+    
+    //const url = `http://localhost:${PORT}/${req.file.path}`
+    res.status(200).send({ message: 'File uploaded successfully',  savedImage });
+  })
+
+  const getImagesII = asyncHandler(async(req, res) => {
+    const images = await Image.findOne().sort({ createdAt: -1 });
+    if(images) {
+        res.status(200).json(images)
+    }
+    else {
+        res.status(400)
+        throw new Error("Images cannot be found")
+    }
+})
+
+
+
+  module.exports = {imageUpload, imageUploadCon,multiImageUpload ,uploadByLink, getAllImages, imageUploadI, imageUploadII, multiImageUploadI, getImagesI, getImagesII, getMultiImage, getMultiImageI}
--- routes/imageRoute.js
+++ routes/imageRoute.js
@@ -2,12 +2,20 @@ const express = require("express")
 const { protect, admin } = require("../middleware/auth")
 const upload = require('../middleware/image');
 const multiUpload = require('../middleware/multiImage')
-const { uploadByLink, imageUpload, multiImageUpload } = require("../controllers/imageController");
+const { uploadByLink, imageUpload, multiImageUpload, getAllImages, imageUploadCon, imageUploadI, imageUploadII, multiImageUploadI, getImagesI, getImagesII, getMultiImage, getMultiImageI } = require("../controllers/imageController");
 const router = express.Router()
 
-router.route('/').get()
+router.route('/').get(getAllImages)
 router.post("/", upload.single('file'),imageUpload)
+router.post("/con", upload.single('file'),imageUploadCon)
 router.post("/multi", multiUpload, multiImageUpload)
+router.get("/multi", getMultiImage)
 router.post("/by_link", uploadByLink)
+router.post("/imageI",upload.single('file'), imageUploadI)
+router.get("/imageI", getImagesI)
+router.post("/imageII",upload.single('file'),  imageUploadII)
+router.get("/imagesII", getImagesII)
+router.post("/imageI/multi", multiUpload, multiImageUploadI)
+router.get("/imageI/multi", getMultiImageI)
 
 module.exports = router
