6a8eaf2d89a28524f6260be3233f8c0d56fdd016
Praises003 <praiseabiodun15@gmail.com>
2024-06-05 13:36:07 +0100

    updates

--- middleware/image.js
+++ middleware/image.js
@@ -4,20 +4,20 @@ const fs = require('fs')
 
 // Multer storage configuration
 const storage = multer.diskStorage({
-  // destination: function (req, file, cb) {
-  //   cb(null, './tmp');
-  // },
   destination: function (req, file, cb) {
-    // Set the destination to the tmp directory relative to the project root
-    const tmpDir = path.join(process.cwd(), 'tmp');
-    
-    // Check if the tmp directory exists, if not, create it
-    if (!fs.existsSync(tmpDir)) {
-      fs.mkdirSync(tmpDir);
-    }
-    
-    cb(null, tmpDir);
+    cb(null, './tmp');
   },
+  // destination: function (req, file, cb) {
+  //   // Set the destination to the tmp directory relative to the project root
+  //   const tmpDir = path.join(process.cwd(), 'tmp');
+    
+  //   // Check if the tmp directory exists, if not, create it
+  //   if (!fs.existsSync(tmpDir)) {
+  //     fs.mkdirSync(tmpDir);
+  //   }
+    
+  //   cb(null, tmpDir);
+  // },
   
   filename: function (req, file, cb) {
     cb(null, file.originalname);
