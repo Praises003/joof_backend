427a96d54928de3c7c6c2eddc9ca89d3114ddb54
Praises003 <praiseabiodun15@gmail.com>
2024-06-17 15:12:45 +0200

    updates

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ const corsOptions = {
    exposedheaders: ["set-cookie"]
  };
 const app = express()
- app.use(cors(corsOptions))
+//app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
