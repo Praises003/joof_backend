387d44b3a3000e7f59139cfc11890b975aadf872
Praises003 <praiseabiodun15@gmail.com>
2024-06-27 19:36:31 +0200

    updates

--- controllers/tableController.js
+++ controllers/tableController.js
@@ -74,4 +74,47 @@ const addTable = asyncHandler(async(req, res) => {
 
 })
 
-module.exports= { getAllTable, reserveSeat, addTable }
+// Update reserved seat information
+const updateReservedSeat = asyncHandler(async (req, res) => {
+  const { tableNumber, seatNumber } = req.params;
+  const { newSeatNumber, newName, newTableName } = req.body;
+
+  try {
+      const table = await Table.findOne({ tableNumber });
+
+      if (!table) {
+          return res.status(404).json({ success: false, message: 'Table not found' });
+      }
+
+      const seat = table.seats.find((s) => s.seatNumber === parseInt(seatNumber));
+
+      if (!seat) {
+          return res.status(404).json({ success: false, message: 'Seat not found' });
+      }
+
+      // Update seatNumber if provided
+      if (newSeatNumber) {
+          seat.seatNumber = newSeatNumber;
+      }
+
+      // Update reservedBy if provided
+      if (newName) {
+          seat.reservedBy = newName;
+      }
+
+      // Update tableName if provided
+      if (newTableName) {
+          seat.tableName = newTableName;
+      }
+
+      await table.save();
+
+      res.status(200).json({ success: true, message: 'Seat reservation updated successfully', seat });
+
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
+
+module.exports= { getAllTable, reserveSeat, addTable, updateReservedSeat }
--- routes/tableRoute.js
+++ routes/tableRoute.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const { protect, admin } = require("../middleware/auth")
-const { getAllTable, reserveSeat, addTable } = require("../controllers/tableController")
+const { getAllTable, reserveSeat, addTable, updateReservedSeat } = require("../controllers/tableController")
 
 
 const router = express.Router()
@@ -8,5 +8,6 @@ const router = express.Router()
 router.get('/', protect, getAllTable)
 router.post('/reserve',protect, reserveSeat)
 router.post('/', protect, addTable)
+router.put('/update-reserve/:tableNumber/:seatNumber', protect, updateReservedSeat);
 
 module.exports = router
